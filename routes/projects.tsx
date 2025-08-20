d7774fcb65e3c50355dd6e42ed59ead91935058f
Daniel Lin <97013614+MisterKermit@users.noreply.github.com>
2025-08-20 12:13:08 -1000

    change the text color

--- routes/projects.tsx
+++ routes/projects.tsx
@@ -19,15 +19,15 @@ export default function Projects() {
 
   return (
     <section>
-      <h1 class="text-3xl mb-6 text-pink-400">Projects 🚀</h1>
+      <h1 class="text-3xl mb-6 text-green-400">Projects 🚀</h1>
       <div class="grid gap-6">
         {projects.map((p) => (
-          <div class="border-2 border-pink-400 rounded-xl p-6 bg-black/70 shadow-[0_0_15px_#ec4899] hover:scale-105 transition-transform">
+          <div class="border-2 border-green-800 rounded-xl p-6 bg-black/70 shadow-[0_0_15px_#ec4899] hover:scale-105 transition-transform">
             <h2 class="text-2xl mb-2 text-green-400">{p.title}</h2>
             <p class="text-lg mb-3">{p.desc}</p>
             <div class="flex flex-wrap gap-2">
               {p.tech.map((t) => (
-                <span class="bg-pink-400 text-black px-2 py-1 rounded-md text-sm">
+                <span class="bg-green-800 text-black px-2 py-1 rounded-md text-sm">
                   {t}
                 </span>
               ))}
