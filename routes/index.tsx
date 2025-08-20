fe040b1f7ceb40b4c07a1a1aba3320d2e8ee8f99
Daniel Lin <97013614+MisterKermit@users.noreply.github.com>
2025-08-20 09:08:31 -1000

    Initial Commit

---
+++ routes/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <body class="bg-cyan-500">
+            <div class="mt-10 px-10 rounded bg-white mx-auto flex max-w-screen-md flex-col justify-center">
+                <div class="mx-auto">
+                    <p class="font-serif text-4xl dark:text-white text-center">Daniel's Portfolio</p>
+                    
+                </div>
+            </div>
+        </body>
+    );
+}
